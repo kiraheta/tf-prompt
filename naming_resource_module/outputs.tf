b95574c367235e462bb156ce8637a3cddec774e1
Kenny Iraheta <iraheta.kenny@gmail.com>
2024-05-29 00:27:26 -0700

    Add header to naming_resource_module outputs.tf

--- naming_resource_module/outputs.tf
+++ naming_resource_module/outputs.tf
@@ -1,3 +1,7 @@
+#---------------------------------------------------
+# naming_resource_module | outputs.tf
+#---------------------------------------------------
+
 output "resource_name" {
     description = "Generated resource_name based from base_name and resource_type."
     value = local.resource_name[var.resource_type]
