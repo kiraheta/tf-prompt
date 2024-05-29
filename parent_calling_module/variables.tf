6cdafeec48cee8c9103c77a9f239dfbc6fb9746f
Kenny Iraheta <iraheta.kenny@gmail.com>
2024-05-29 00:29:45 -0700

    Add variable resource map to parent_calling_module variables.tf

--- parent_calling_module/variables.tf
+++ parent_calling_module/variables.tf
@@ -1,3 +1,8 @@
 #---------------------------------------------------
 # parent_calling_module | variables.tf
 #---------------------------------------------------
+
+variable "resource_map" {
+    type = map(string)
+    description = "Resource map for base name as key and resource type as value."
+}
